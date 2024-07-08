1ef7c325f32f958692fa197e8a2441cb8722305a
JoseSandovalVilla <jsvilla776@gmail.com>
2024-07-07 20:24:45 -0600

    Verificacion de que la api funciona correctamente

---
+++ src/main/java/com/example/JoseTarea/JoseTareaApplication.java
@@ -0,0 +1,13 @@
+package com.example.JoseTarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoseTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoseTareaApplication.class, args);
+	}
+
+}
